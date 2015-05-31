be0de0768d1f4805f5872b821145d6cf6b4e5ba1
Kirill Pimenov <kirill@pimenov.cc>
2015-05-31 19:18:59 +0200

    First TDD steps for Rails app checkout

--- roles/critical_chain/spec/critical_chain_spec.rb
+++ roles/critical_chain/spec/critical_chain_spec.rb
@@ -1,1 +1,15 @@
 require "spec_helper"
+
+describe 'critical_chain app' do
+  let(:deploy_path) { '/var/www/critical_chain' }
+  let(:github_repo_path) { 'git@github.com:kirushik/critical_chain.git' }
+  let(:app_user) { 'critical_chain' }
+
+  it "should checkout a github repo" do
+    expect(file(deploy_path)).to be_directory
+    expect(file(deploy_path)).to be_owned_by app_user
+
+    expect(file("#{deploy_path}/.git")).to be_directory
+    expect(file("#{deploy_path}/.git/config")).to contain "url = #{github_repo_path}"
+  end
+end
