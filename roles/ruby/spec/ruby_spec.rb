e8d531f2fbe5482e10bbb2824905dcd535d5ef7e
Kirill Pimenov <kirill@pimenov.cc>
2015-05-31 21:10:00 +0200

    Ruby2.2 also installs bundler

--- roles/ruby/spec/ruby_spec.rb
+++ roles/ruby/spec/ruby_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper.rb'
 
-describe "ruby 2.2" do
-  it "should be installed from PPA" do
+describe 'ruby 2.2' do
+  it 'should be installed from PPA' do
     expect(ppa('ppa:brightbox/ruby-ng')).to be_enabled
     expect(package('ruby2.2')).to be_installed
     expect(command('ruby --version').stdout).to contain 'ruby 2.2'
+  end
+
+  it 'should also install bundler' do
+    expect(command('bundle --version').exit_status).to eq 0
   end
 end
