ced1cd35ba40b7ac58df21523f65a8b882bea84f
Kirill Pimenov <kirill@pimenov.cc>
2015-05-31 20:00:35 +0200

    Explicit Ruby version check

--- roles/ruby/spec/ruby_spec.rb
+++ roles/ruby/spec/ruby_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper.rb'
 
-describe "latest ruby" do
+describe "ruby 2.2" do
   it "should be installed from PPA" do
     expect(ppa('ppa:brightbox/ruby-ng')).to be_enabled
     expect(package('ruby2.2')).to be_installed
+    expect(command('ruby --version').stdout).to contain 'ruby 2.2'
   end
 end
